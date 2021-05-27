a909670a971dfcf61fa3cc7d213b1283559a35ef
Sheila Ababio <sheilaababio@yahoo.co.uk>
2021-05-27 17:49:01 +0100

    update code

---
+++ code,R.R
@@ -0,0 +1,8 @@
+###code>R
+install.packages("dslabs")  # to install a single package
+install.packages("tidyverse")
+install.packages("dplyr")
+installed.packages() # to see the list of all installed packages
+library("dslabs")
+library("tidyverse")
+library("dplyr")
